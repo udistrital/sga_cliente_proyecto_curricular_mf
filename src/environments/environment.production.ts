836292811fa9b75845863b2d36db77e8eae7d654
bfmunozm <swsbmm@gmail.com>
2024-05-03 10:02:37 -0500

    feat: Actualizar URL del servicio OIKOS en los archivos de entorno udistrital/sga_documentacion#197

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -12,6 +12,6 @@ export const environment = {
   TERCEROS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_crud/v1/',
   DOCUMENTO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v2/',
   CORE_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/core_crud/v2/',
-  OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v1/',
+  OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v2/',
   PROYECTO_ACADEMICO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/proyecto_academico_crud/v1/',
 };
