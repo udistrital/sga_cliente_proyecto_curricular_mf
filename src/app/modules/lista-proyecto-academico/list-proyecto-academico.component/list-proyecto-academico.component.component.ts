4e15ee57a40ceec5b4cfbbf358a71a9fdb07b56a
CrisEs2506 <cristianespitia2506@gmail.com>
2026-02-18 02:08:46 -0500

    feature: Traer el ultimo registro calificado y no anteriores

--- src/app/modules/lista-proyecto-academico/list-proyecto-academico.component/list-proyecto-academico.component.component.ts
+++ src/app/modules/lista-proyecto-academico/list-proyecto-academico.component/list-proyecto-academico.component.component.ts
@@ -334,8 +334,8 @@ export class ListProyectoAcademicoComponent implements OnInit {
       (res: DetalleProyectoAcademico | null) => {
         if (res == null) throw new Error('No se encontró el proyecto');
         try {
-          const registroCalificado = res.Registro?.find(r => r.TipoRegistroId.CodigoAbreviacion === 'REGACR');
-          const registroAltaCalidad = res.Registro?.find(r => r.TipoRegistroId.CodigoAbreviacion === 'REGREN');
+          const registroCalificado = res.Registro?.filter(r => r.TipoRegistroId.CodigoAbreviacion === 'REGACR').sort((a, b) => b.Id - a.Id)[0];
+          const registroAltaCalidad = res.Registro?.filter(r => r.TipoRegistroId.CodigoAbreviacion === 'REGREN').sort((a, b) => b.Id - a.Id)[0];
 
           const parseVigencia = (texto: string) => {
             if (!texto) return { meses: '', anos: '' };
